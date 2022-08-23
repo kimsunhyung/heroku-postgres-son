064b029b210fcede4a4cc474bbc9b64e264f1904
kimsunhyung <326ksh@gmail.com>
2022-08-23 16:56:20 +0900

    updated

--- database.py
+++ database.py
@@ -36,9 +36,8 @@ def area(loc):
     cur.execute("select * from public.area where location like '%{0}%';".format(loc))
     result = cur.fetchall()
     print(result)
-    my_df = pd.DataFrame(result) 
-    area_df = my_df.columns = [desc[0] for desc in cur.description]
+    my_df = pd.DataFrame(result, column = ['name', 'division','location','notice_date','start_day','end_day','release_date','rink']) 
     #쿼리(평택)
-    return area_df
+    return my_df
     print(area())
 
